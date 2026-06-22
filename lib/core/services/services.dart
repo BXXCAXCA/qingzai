9f577657c10fef2ea72c50e2fa47ced9c0d85281
BXXCAXCA <155696230+BXXCAXCA@users.noreply.github.com>
2026-06-23 00:18:37 +0800

    chore: export socket LAN transfer service

--- lib/core/services/services.dart
+++ lib/core/services/services.dart
@@ -5,6 +5,7 @@ export 'hive_model_adapters.dart';
 export 'hive_storage_service.dart';
 export 'lan_transfer_service.dart';
 export 'platform_service.dart';
+export 'socket_lan_transfer_service.dart';
 export 'storage_service.dart';
 export 'version_service.dart';
 export 'webdav_service.dart';
