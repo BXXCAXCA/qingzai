fff1d899264a0b6a3c1a27110fc32129cb7be376
BXXCAXCA <155696230+BXXCAXCA@users.noreply.github.com>
2026-06-22 23:28:43 +0800

    feat: register hive storage provider

--- lib/core/providers/service_providers.dart
+++ lib/core/providers/service_providers.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../services/aes_gcm_encryption_service.dart';
 import '../services/encryption_service.dart';
+import '../services/hive_storage_service.dart';
 import '../services/lan_transfer_service.dart';
 import '../services/platform_service.dart';
 import '../services/storage_service.dart';
@@ -10,7 +11,7 @@ import '../services/webdav_service.dart';
 import '../sync/sync_manager.dart';
 
 final storageServiceProvider = Provider<StorageService>((ref) {
-  throw UnimplementedError('StorageService implementation is not registered.');
+  return HiveStorageService();
 });
 
 final encryptionServiceProvider = Provider<EncryptionService>((ref) {
