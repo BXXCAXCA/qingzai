3e9255f70437e0a91ba51b33e4787502c8b2a98a
BXXCAXCA <155696230+BXXCAXCA@users.noreply.github.com>
2026-06-22 17:12:15 +0800

    feat: add app info constants

---
+++ lib/core/constants/app_info.dart
@@ -0,0 +1,8 @@
+class AppInfo {
+  const AppInfo._();
+
+  static const name = '轻载';
+  static const englishName = 'Qing Zai';
+  static const packageName = 'com.qingzai.app';
+  static const initialVersion = '0.1.0';
+}
