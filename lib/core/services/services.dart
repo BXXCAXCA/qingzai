f7dd7fc4b8917b72ae42aca3a931e8a60dd6cf05
BXXCAXCA <155696230+BXXCAXCA@users.noreply.github.com>
2026-06-22 18:18:52 +0800

    chore: add core engineering skeleton

---
+++ lib/core/services/services.dart
@@ -0,0 +1,6 @@
+export 'encryption_service.dart';
+export 'lan_transfer_service.dart';
+export 'platform_service.dart';
+export 'storage_service.dart';
+export 'version_service.dart';
+export 'webdav_service.dart';
