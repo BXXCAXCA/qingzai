f46c32d7f0b31c334087541d95552cc9e58c3507
BXXCAXCA <155696230+BXXCAXCA@users.noreply.github.com>
2026-06-23 14:50:39 +0800

    fix: restore sha256 test expectation

--- test/core/services/aes_gcm_encryption_service_test.dart
+++ test/core/services/aes_gcm_encryption_service_test.dart
@@ -120,7 +120,7 @@ void main() {
 
       expect(
         service.calculateSha256(utf8.encode('hello')),
-        '2cf24dba5fb0a30e26e83e29e1b161e5c1fa7425e73043362938b9824'.replaceFirst('e83e', 'e83e'),
+        '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
       );
     });
   });
